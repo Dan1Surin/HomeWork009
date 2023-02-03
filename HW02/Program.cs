fe463daa0e9943a0c6cc1f481b80364b13b69abd
Даниил Сурин <dan1surin@gmail.com>
2023-02-03 15:36:08 +0300

    1

---
+++ HW02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в
+//  промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int M = 5;
+int N = 100;
+int SUM=0;
+
+
+void N1numbers(int m, int n,int sum)
+{
+    sum=sum+m;
+    if(m==n)  {Console.WriteLine(sum); return;}
+    N1numbers(m+1,n,sum);
+}
+
+N1numbers(M,N,SUM);
---
+++ HW03/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных 
+// числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 9
+
+int M = 5;
+int N = 100;
+int SUM=0;
+
+
+void N1numbers(int m, int n,int sum)
+{
+    sum=sum+m;
+    if(m==n)  {Console.WriteLine(sum); return;}
+    N1numbers(m+1,n,sum);
+}
+
+N1numbers(M,N,SUM);
